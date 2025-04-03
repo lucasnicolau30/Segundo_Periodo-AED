8ed13a7a219366dce956ed12578941b5160e053a
Lucas Nicolau <nicolau.lucas04@gmail.com>
2025-04-03 12:38:21 -0400

    Update triangulo.c

--- Lab 7 (tipos estruturados)/triangulo.c
+++ Lab 7 (tipos estruturados)/triangulo.c
@@ -4,5 +4,39 @@
 typedef struct{ 
     float a; float b; float c; }Triangulo;
 
+int verifica_triangulo(Triangulo* t){ 
+    if(t -> a <= 0 || t -> b <= 0 || t -> c <= 0 || (t -> a + t -> b <= t -> c) || (t -> a + t -> c <= t -> b) || (t-> c + t -> b <= t -> a)){ 
+        return 0; 
+    } 
+    if(t -> a == t -> b && t -> b == t -> c){ 
+        return 1;
+    } 
+    else if(t -> a == t -> b || t -> b == t -> c || t -> a ==t -> c){ 
+        return 2; 
+    } 
+    else{ 
+        return 3; 
+    } 
+} 
+
+int main() {
+    Triangulo t; 
+    while(scanf("%f %f %f" , &t.a , &t.b , &t.c) != EOF){
+        int tipo = verifica_triangulo(&t); 
+        
+        if(tipo == 0){ 
+            printf("triangulo inexistente \n"); 
+        } 
+        else if(tipo == 1){ 
+            printf("triangulo equilatero \n"); 
+        } 
+        else if(tipo == 2){ 
+            printf("triangulo isosceles \n"); 
+        } 
+        else{ 
+            printf("triangulo escaleno \n"); 
+        } 
+    } 
     
-int verifica_triangulo(Triangulo* t){ if(t -> a <= 0 || t -> b <= 0 || t -> c <= 0 || (t -> a + t -> b <= t -> c) || (t -> a + t -> c <= t -> b) || (t-> c + t -> b <= t -> a)){ return 0; } if(t -> a == t -> b && t -> b == t -> c){ return 1; } else if(t -> a == t -> b || t -> b == t -> c || t -> a ==t -> c){ return 2; } else{ return 3; } } int main() {Triangulo t; while(scanf("%f %f %f" , &t.a , &t.b , &t.c) != EOF){int tipo = verifica_triangulo(&t); if(tipo == 0){ printf("triangulo inexistente \n"); } else if(tipo == 1){ printf("triangulo equilatero \n"); } elseif(tipo == 2){ printf("triangulo isosceles \n"); } else{ printf("triangulo escaleno \n"); } } return 0; }
+    return 0; 
+}
